97a12cdcbfd652b9c3587f1b539b2bb0f2200a5c
nataraj.ramamoorthy <nataraj.ramamoorthy13@gmail.com>
2023-12-26 22:40:00 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -17,21 +17,3 @@ variable "gcr_region" {
   description = "Name of the GCP region where the GCR registry is located. e.g: 'us' or 'eu'."
   type        = string
 }
-
-
-# ---------------------------------------------------------------------------------------------------------------------
-# OPTIONAL PARAMETERS
-# ---------------------------------------------------------------------------------------------------------------------
-
-
-variable "repository_name" {
-  description = "Name of the Google Cloud Source Repository to create."
-  type        = string
-  default     = "sample-docker-app"
-}
-
-variable "branch_name" {
-  description = "Example branch name used to trigger builds."
-  type        = string
-  default     = "master"
-}
